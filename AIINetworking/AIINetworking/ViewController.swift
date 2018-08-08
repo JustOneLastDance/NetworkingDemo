663d284b9b0b0613962d903822f247cde75450cf
JustinChou <justinchou186@gmail.com>
2018-08-08 12:30:46 +0800

    initialize the project

---
+++ AIINetworking/AIINetworking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AIINetworking
+//
+//  Created by JustinChou on 2018/8/8.
+//  Copyright © 2018 JustinChou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
